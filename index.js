b60b6db62814fa0c136a318ad024d1d0bd3d1f4f
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-28 13:54:30 -0400

    Scraped instagram user profile data.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const puppeteer = require('puppeteer');
-const secrets = require('./secrets');
+const puppeteer = require("puppeteer");
+const secrets = require("./secrets");
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://instagram.com');
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://instagram.com");
 
-    await page.waitForSelector('input');
+  await page.waitForSelector("input");
 
-    const inputs = await page.$$('input');
+  const inputs = await page.$$("input");
 
-    await inputs[0].type(secrets.USERNAME);
-    await inputs[1].type(secrets.PASSWORD);
+  await inputs[0].type(secrets.USERNAME);
+  await inputs[1].type(secrets.PASSWORD);
 
-    const loginButton = (await page.$$('button'))[1];
-    await loginButton.click();
+  const loginButton = (await page.$$("button"))[1];
+  await loginButton.click();
 
-    await page.waitForNavigation();
+  await page.waitForNavigation();
 
-    const userNames = ['akhilgoyal_', 'garimachawla1997', 'familytimes_'];
+  const userNames = ["akhilgoyal_", "garimachawla1997", "familytimes_"];
 
-    for (let username of userNames) {
-        await page.goto(`https://instagram.com/${username}`);
-    }
+  for (let username of userNames) {
+    await page.goto(`https://instagram.com/${username}`);
+    await page.waitForSelector("img");
 
-    await browser.close();
+    const imgSrc = await page.$eval("img", (el) => el.getAttribute("src"));
+    const headerData = await page.$eval("header li", (els) =>
+      els.map((el) => el.textContent)
+    );
+    const name = await page.$eval("header h1", (el) => el.textContent);
+    const desc = await page.$$eval("span", (els) => els[4].textContent);
+    const profile = { imgSrc, headerData, name, desc };
 
+    console.log({ profile });
+  }
+
+  await browser.close();
 })();
